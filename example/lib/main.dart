b48af098f5da7823f893e4beb7953f6f26b8f0b5
Fekz115 <fekz115@gmail.com>
2021-05-27 20:45:00 +0300

    feat(example): add example project

---
+++ example/lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:whelm/whelm.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreWidget<int, int, String>(
+      initaialState: 0,
+      reducers: [
+        (state, action) {
+          return state + action;
+        }
+      ],
+      middlewares: [
+        (state, action, actionDispatcher, eventDispatcher) {
+          eventDispatcher('action: $action');
+        }
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              StoreConnection<int, int, String, int>(
+                builder: (context, state, dispatcher) {
+                  return Text(
+                    '$state',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                },
+                connect: (state) => state,
+                eventListener: (context, event) async =>
+                    ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('$event'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: StoreConnection<int, int, String, int>(
+          builder: (context, state, dispatcher) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FloatingActionButton(
+                  onPressed: () => dispatcher(1),
+                  tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                FloatingActionButton(
+                  onPressed: () => dispatcher(-1),
+                  tooltip: 'Decrement',
+                  child: Icon(Icons.remove),
+                ),
+              ],
+            );
+          },
+          connect: (state) => state,
+        ),
+      ),
+    );
+  }
+}
